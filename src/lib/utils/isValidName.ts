313574a9c2585aa876525d7a552e5c4a9486f450
Giacomo Triggiano <giacomotriggiano@gmail.com>
2018-12-16 18:14:44 +0100

    refactor(src/lib/utils/isValidName): implement a better isValidName

--- src/lib/utils/isValidName.ts
+++ src/lib/utils/isValidName.ts
@@ -1,7 +1,7 @@
 import { isString } from 'lodash'
 
 export function isValidName(x: any): boolean {
-  return isString(x) && /^[a-zA-Z]([a-zA-Z0-9]).*?/.test(x)
+  return isString(x) && /^[a-zA-Z]\w*$/.test(x)
 }
 
-export const validNameDescription = `string starting with a letter and composed by letters and digits only`
+export const validNameDescription = `string starting with a letter and composed by letters, numbers and underscores only`
