b47b15f7844fe8a70aa9655f7f6c69d3ea3c4e21
Giacomo Triggiano <giacomotriggiano@gmail.com>
2018-12-18 00:52:13 +0100

    chore: update project tests config

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['dist/main/lib/**/*'],
-  coverageDirectory: 'pages/coverage',
-  coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  coverageThreshold: {
-    global: {
-      lines: 100,
-      statements: 100,
-      functions: 100,
-      branches: 100,
-    },
-  },
+  coverageReporters: ['json'],
+  // coverageThreshold: {
+  //   global: {
+  //     lines: 100,
+  //     statements: 100,
+  //     functions: 100,
+  //     branches: 100,
+  //   },
+  // },
   reporters: ['default', 'jest-stare'],
   roots: ['<rootDir>/src/lib'],
   testMatch: ['**/__tests__/**/?(*.)+(spec|test).js'],
   testResultsProcessor: './node_modules/jest-stare',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
+  // moduleFileExtensions: ['ts', 'js'],
+  // transform: {
+  //   '^.+\\.(js)$': 'babel-jest',
+  //   '^.+\\.(ts)$': 'ts-jest',
+  // },
 }
